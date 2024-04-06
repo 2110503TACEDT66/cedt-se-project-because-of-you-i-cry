5af9304b4def4ca3bd4291d300c7aa42403dffcd
Enteryouremail <chanaya2548@gmail.com>
2024-04-06 23:21:55 +0700

    layout

--- Frontend/src/app/profile/information/page.tsx
+++ Frontend/src/app/profile/information/page.tsx
@@ -25,6 +25,7 @@ export default function Profile() {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
   const [statusName, setStatusName] = useState(true);
   const [statusEmail, setStatusEmail] = useState(true);
+  const [statusTel, setStatusTel] = useState(true);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [tel, setTel] = useState("");
@@ -54,7 +55,7 @@ export default function Profile() {
   if (!name && !email && !tel) return null;
 
   return (
-    <div className="w-full h-[100vh] flex flex-row justtify-center items-center">
+    <div className="w-full h-full flex flex-row justtify-center items-center">
       <Suspense
         fallback={
           <p className="m-10">
@@ -63,135 +64,156 @@ export default function Profile() {
           </p>
         }
       >
-        <div className="w-[30%] h-full bg-slate-200 flex flex-col items-center py-10 space-y-5 relative">
-          <div className="h-[30%] w-[60%] relative">
-            <Image
-              src={"/img/logo2.png"}
+        <div className="w-full h-full text-center mx-auto mt-36 items-center flex flex-col ">
+          <div className="text-4xl ml-4 mb-6">Your Profile</div>
+          <div className="flex items-center justify-center">
+            <img
+              src="/img/logo2.png"
               alt="profile picture"
-              objectFit="cover"
-              width={300}
-              height={300}
+              className="w-[60%]"
             />
           </div>
-          <div className="h-[5%] w-[90%] relative">
-            <div className="text-3xl text-center mt-12">{name}</div>
-          </div>
-          <div className="h-[20%] w-[95%] space-y-2 flex-1 itemscenter relative">
-            <Link href={"/profile/information"}>
-              <div className="pb-2 pl-5 border-b border-black mt-16 hover:text-bold hover:border-b-2">
-                Profile
-              </div>
-            </Link>
-            <Link href={"/profile/booking"}>
-              <div className="pb-2 pl-5 border-b border-black mt-5 hover:text-bold hover:border-b-2">
-                Booking
-              </div>
-            </Link>
-          </div>
-          <div className="h-[75%] w-[95%] flex items-end">
-            <div
-              className="w-[100%] pb-2 pl-5 border-b border-black flex flex-row bottom-4 cursor-pointer hover:text-bold hover:border-b-2"
-              onClick={() => setShowLogoutModal(true)}
-            >
-              Logout
-              <Image
-                src={"/img/logoutprofile.png"}
-                alt="logout logo"
-                width={20}
-                height={10}
-                className="ml-2 scale-75"
-              />
-            </div>
-          </div>
-        </div>
-        <LogoutModal
-          isOpen={showLogoutModal}
-          onClose={() => setShowLogoutModal(false)}
-        />
 
-        <div className="w-[70%] h-full text-left mx-20 mt-48">
-          <div className="text-2xl ml-4 mb-12">Profile</div>
-          <div className="flex flex-col space-y-8 m-4 mt-50">
-            <div className="space-y-3">
-              <div>Name</div>
+          <div className="flex flex-col space-y-4 m-4 mt-50">
+            <div className="space-y-1">
+              <div className="text-left ml-1 text-lg">Name</div>
               <div className="flex flex-row">
-                <TextField
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                  value={name}
-                  size="small"
-                  InputProps={{
-                    readOnly: statusName,
-                    style: { width: "300px" },
-                  }}
-                />
-
-                <Image
-                  onClick={() => {
-                    setStatusName(!statusName);
-                  }}
-                  src={"/img/edit.jpg"}
-                  alt="edit"
-                  objectFit="cover"
-                  width={40}
-                  height={40}
-                  className={
-                    !statusName
-                      ? "ml-2 animate-pulse active:scale-75"
-                      : "ml-2 hover:opacity-50 active:scale-75"
-                  }
-                />
+                <div className="w-[90%]">
+                  <TextField
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                    value={name}
+                    size="small"
+                    InputProps={{
+                      readOnly: statusName,
+                      style: { width: "300px" },
+                    }}
+                  />
+                </div>
+                <div className="w-[10%] flex flex-row items-end ml-1.5">
+                  <svg
+                    width="24"
+                    height="25"
+                    viewBox="0 0 24 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={
+                      !statusName
+                        ? "ml animate-pulse active:scale-75"
+                        : "ml hover:opacity-50 active:scale-75"
+                    }
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M13.8787 4.08205C15.0503 2.91048 16.9497 2.91047 18.1213 4.08205L20.2929 
+                      6.25362C21.4645 7.4252 21.4645 9.32469 20.2929 10.4963L11.4142 19.3749H21C21.5523 
+                      19.3749 22 19.8227 22 20.3749C22 20.9272 21.5523 21.3749 21 21.3749H4C3.44772 21.3749 3 
+                      20.9272 3 20.3749V15.3749C3 15.1097 3.10536 14.8554 3.29289 14.6678L13.8787 4.08205ZM8.58579 
+                      19.3749L16.5858 11.3749L13 7.78916L5 15.7892V19.3749H8.58579ZM14.4142 6.37494L18 9.96073L18.8787
+                      9.08205C19.2692 8.69152 19.2692 8.05836 18.8787 7.66784L16.7071 5.49626C16.3166 5.10574 15.6834 
+                      5.10574 15.2929 5.49626L14.4142 6.37494Z"
+                      fill="black"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-            <div className="space-y-3">
-              <div>Email</div>
+            <div className="space-y-1">
+              <div className="ml-1 text-left text-lg">Email</div>
               <div className="flex flex-row">
-                <TextField
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                  value={email}
-                  size="small"
-                  InputProps={{
-                    readOnly: statusEmail,
-                    style: { width: "300px" },
-                  }}
-                />
-                <Image
-                  onClick={() => {
-                    setStatusEmail(!statusEmail);
-                  }}
-                  src={"/img/edit.jpg"}
-                  alt="edit"
-                  objectFit="cover"
-                  width={40}
-                  height={40}
-                  className={
-                    !statusEmail
-                      ? "ml-2 animate-pulse active:scale-75"
-                      : "ml-2 hover:opacity-50 active:scale-75"
-                  }
-                />
+                <div className="w-[90%]">
+                  <TextField
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    value={email}
+                    size="small"
+                    InputProps={{
+                      readOnly: statusEmail,
+                      style: { width: "300px" },
+                    }}
+                  />
+                </div>
+                <div className="w-[10%] flex flex-row items-end ml-1.5">
+                  <svg
+                    width="24"
+                    height="25"
+                    viewBox="0 0 24 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={
+                      !statusName
+                        ? "ml animate-pulse active:scale-75"
+                        : "ml hover:opacity-50 active:scale-75"
+                    }
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M13.8787 4.08205C15.0503 2.91048 16.9497 2.91047 18.1213 4.08205L20.2929 
+                      6.25362C21.4645 7.4252 21.4645 9.32469 20.2929 10.4963L11.4142 19.3749H21C21.5523 
+                      19.3749 22 19.8227 22 20.3749C22 20.9272 21.5523 21.3749 21 21.3749H4C3.44772 21.3749 3 
+                      20.9272 3 20.3749V15.3749C3 15.1097 3.10536 14.8554 3.29289 14.6678L13.8787 4.08205ZM8.58579 
+                      19.3749L16.5858 11.3749L13 7.78916L5 15.7892V19.3749H8.58579ZM14.4142 6.37494L18 9.96073L18.8787
+                      9.08205C19.2692 8.69152 19.2692 8.05836 18.8787 7.66784L16.7071 5.49626C16.3166 5.10574 15.6834 
+                      5.10574 15.2929 5.49626L14.4142 6.37494Z"
+                      fill="black"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
-            <div className="space-y-3">
-              <div>Tel</div>
+            <div className="space-y-1">
+              <div className="ml-1 text-left text-lg">Tel.</div>
               <div className="flex flex-row">
-                <TextField
-                  defaultValue={tel}
-                  size="small"
-                  disabled
-                  InputProps={{
-                    style: { width: "300px" },
-                  }}
-                />
+                <div className="w-[90%]">
+                  <TextField
+                    onChange={(e) => {
+                      setTel(e.target.value);
+                    }}
+                    value={tel}
+                    size="small"
+                    InputProps={{
+                      readOnly: statusTel,
+                      style: { width: "300px" },
+                    }}
+                  />
+                </div>
+                <div className="w-[10%] flex flex-row items-end ml-1.5">
+                  <svg
+                    width="24"
+                    height="25"
+                    viewBox="0 0 24 25"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className={
+                      !statusName
+                        ? "ml animate-pulse active:scale-75"
+                        : "ml hover:opacity-50 active:scale-75"
+                    }
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M13.8787 4.08205C15.0503 2.91048 16.9497 2.91047 18.1213 4.08205L20.2929 
+                      6.25362C21.4645 7.4252 21.4645 9.32469 20.2929 10.4963L11.4142 19.3749H21C21.5523 
+                      19.3749 22 19.8227 22 20.3749C22 20.9272 21.5523 21.3749 21 21.3749H4C3.44772 21.3749 3 
+                      20.9272 3 20.3749V15.3749C3 15.1097 3.10536 14.8554 3.29289 14.6678L13.8787 4.08205ZM8.58579 
+                      19.3749L16.5858 11.3749L13 7.78916L5 15.7892V19.3749H8.58579ZM14.4142 6.37494L18 9.96073L18.8787
+                      9.08205C19.2692 8.69152 19.2692 8.05836 18.8787 7.66784L16.7071 5.49626C16.3166 5.10574 15.6834 
+                      5.10574 15.2929 5.49626L14.4142 6.37494Z"
+                      fill="black"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
             <div>
               <button
-                className="rounded-md bg-orange-600 hover:bg-orange-400 px-8 py-2
-              shadow-sm text-white right-0 bottom-0 mt-8"
+                className="rounded-md bg-[#285F3E] hover:bg-green-600 px-8 py-2
+              shadow-sm text-white text-bold right-0 bottom-0 mt-4"
                 onClick={() => {
                   if (id && session.data.user.token) {
                     updateUser(id, name, email, session.data.user.token);
@@ -208,47 +230,3 @@ export default function Profile() {
     </div>
   );
 }
-
-/*
-    <div className="w-[100%] flex flex-col items-center space-y-4 min-h-[70vh] justify-center">
-      <div className="border border-black p-10 rounded-xl">
-        <div className="text-3xl font-medium"> Profile </div>
-        <br />
-
-        {
-          <Link href={"/campground"}>
-            <button
-              className="block rounded-md bg-orange-600 hover:bg-orange-400 px-3 py-2
-              shadow-sm text-white "
-              >
-              Book campground
-            </button>
-          </Link>
-        }
-        <div className="flex flex-col">
-          <div className="w-200 h-20 border border-gray-500 p-5 m-5 flex items-center justify-center">
-            {" "}
-            Name : {profile.data.name} <br />
-          </div>
-          <div className="w-200 h-20 border border-gray-500 p-5 m-5 flex items-center justify-center">
-            {" "}
-            Email : {profile.data.email} <br />
-          </div>
-          <div className="w-200 h-20 border border-gray-500 p-5 m-5 flex items-center justify-center">
-            {" "}
-            Telephone : {profile.data.tel} <br />
-          </div>
-        </div>
-        <div>
-          <Link href="/api/auth/logout">
-            <button
-              className="rounded-md bg-orange-600 hover:bg-orange-400 px-3 py-2
-              shadow-sm text-white right-0 bottom-0"
-              >
-              Logout
-            </button>
-          </Link>
-        </div>
-      </div>
-    </div>
-*/
