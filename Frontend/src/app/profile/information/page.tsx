b35bb19ce47af15c26780f270515ffc8629d914f
Enteryouremail <chanaya2548@gmail.com>
2024-04-07 15:49:20 +0700

    function complete?

--- Frontend/src/app/profile/information/page.tsx
+++ Frontend/src/app/profile/information/page.tsx
@@ -64,7 +64,7 @@ export default function Profile() {
           </p>
         }
       >
-        <div className="w-full h-full text-center mx-auto mt-36 items-center flex flex-col ">
+        <div className="w-full h-full text-center mx-auto mt-20 items-center flex flex-col ">
           <div className="text-4xl ml-4 mb-6">Your Profile</div>
           <div className="flex items-center justify-center">
             <img
@@ -86,37 +86,29 @@ export default function Profile() {
                     value={name}
                     size="small"
                     InputProps={{
-                      readOnly: statusName,
-                      style: { width: "300px" },
+                      disabled: statusName,
+                      style: {
+                        width: "300px",
+                        backgroundColor: statusName ? "#E7E7E7" : "#FFFFFF",
+                      },
                     }}
                   />
                 </div>
-                <div className="w-[10%] flex flex-row items-end ml-1.5">
-                  <svg
-                    width="24"
-                    height="25"
-                    viewBox="0 0 24 25"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                <div className="w-[10%] flex flex-row items-end ml-2">
+                  <Image
+                    src={"/img/edit.png"}
+                    alt="edit name"
+                    width={24}
+                    height={25}
                     className={
                       !statusName
-                        ? "ml animate-pulse active:scale-75"
-                        : "ml hover:opacity-50 active:scale-75"
+                        ? "scale-75 ml animate-pulse active:scale-50"
+                        : "scale-75 ml hover:opacity-50 active:scale-50"
                     }
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M13.8787 4.08205C15.0503 2.91048 16.9497 2.91047 18.1213 4.08205L20.2929 
-                      6.25362C21.4645 7.4252 21.4645 9.32469 20.2929 10.4963L11.4142 19.3749H21C21.5523 
-                      19.3749 22 19.8227 22 20.3749C22 20.9272 21.5523 21.3749 21 21.3749H4C3.44772 21.3749 3 
-                      20.9272 3 20.3749V15.3749C3 15.1097 3.10536 14.8554 3.29289 14.6678L13.8787 4.08205ZM8.58579 
-                      19.3749L16.5858 11.3749L13 7.78916L5 15.7892V19.3749H8.58579ZM14.4142 6.37494L18 9.96073L18.8787
-                      9.08205C19.2692 8.69152 19.2692 8.05836 18.8787 7.66784L16.7071 5.49626C16.3166 5.10574 15.6834 
-                      5.10574 15.2929 5.49626L14.4142 6.37494Z"
-                      fill="black"
-                    />
-                  </svg>
+                    onClick={() => {
+                      setStatusName(!statusName);
+                    }}
+                  />
                 </div>
               </div>
             </div>
@@ -131,37 +123,29 @@ export default function Profile() {
                     value={email}
                     size="small"
                     InputProps={{
-                      readOnly: statusEmail,
-                      style: { width: "300px" },
+                      disabled: statusEmail,
+                      style: {
+                        width: "300px",
+                        backgroundColor: statusEmail ? "#E7E7E7" : "#FFFFFF",
+                      },
                     }}
                   />
                 </div>
-                <div className="w-[10%] flex flex-row items-end ml-1.5">
-                  <svg
-                    width="24"
-                    height="25"
-                    viewBox="0 0 24 25"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                <div className="w-[10%] flex flex-row items-end ml-2">
+                  <Image
+                    src={"/img/edit.png"}
+                    alt="edit email"
+                    width={24}
+                    height={25}
                     className={
-                      !statusName
-                        ? "ml animate-pulse active:scale-75"
-                        : "ml hover:opacity-50 active:scale-75"
+                      !statusEmail
+                        ? "scale-75 ml animate-pulse active:scale-50"
+                        : "scale-75 ml hover:opacity-50 active:scale-50"
                     }
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M13.8787 4.08205C15.0503 2.91048 16.9497 2.91047 18.1213 4.08205L20.2929 
-                      6.25362C21.4645 7.4252 21.4645 9.32469 20.2929 10.4963L11.4142 19.3749H21C21.5523 
-                      19.3749 22 19.8227 22 20.3749C22 20.9272 21.5523 21.3749 21 21.3749H4C3.44772 21.3749 3 
-                      20.9272 3 20.3749V15.3749C3 15.1097 3.10536 14.8554 3.29289 14.6678L13.8787 4.08205ZM8.58579 
-                      19.3749L16.5858 11.3749L13 7.78916L5 15.7892V19.3749H8.58579ZM14.4142 6.37494L18 9.96073L18.8787
-                      9.08205C19.2692 8.69152 19.2692 8.05836 18.8787 7.66784L16.7071 5.49626C16.3166 5.10574 15.6834 
-                      5.10574 15.2929 5.49626L14.4142 6.37494Z"
-                      fill="black"
-                    />
-                  </svg>
+                    onClick={() => {
+                      setStatusEmail(!statusEmail);
+                    }}
+                  />
                 </div>
               </div>
             </div>
@@ -176,37 +160,29 @@ export default function Profile() {
                     value={tel}
                     size="small"
                     InputProps={{
-                      readOnly: statusTel,
-                      style: { width: "300px" },
+                      style: {
+                        width: "300px",
+                        backgroundColor: statusTel ? "#E7E7E7" : "#FFFFFF",
+                      },
+                      disabled: statusTel,
                     }}
                   />
                 </div>
-                <div className="w-[10%] flex flex-row items-end ml-1.5">
-                  <svg
-                    width="24"
-                    height="25"
-                    viewBox="0 0 24 25"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                <div className="w-[10%] flex flex-row items-end ml-2">
+                  <Image
+                    src={"/img/edit.png"}
+                    alt="edit Telephone"
+                    width={24}
+                    height={25}
                     className={
-                      !statusName
-                        ? "ml animate-pulse active:scale-75"
-                        : "ml hover:opacity-50 active:scale-75"
+                      !statusTel
+                        ? "scale-75 ml animate-pulse active:scale-50"
+                        : "scale-75 ml hover:opacity-50 active:scale-50"
                     }
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M13.8787 4.08205C15.0503 2.91048 16.9497 2.91047 18.1213 4.08205L20.2929 
-                      6.25362C21.4645 7.4252 21.4645 9.32469 20.2929 10.4963L11.4142 19.3749H21C21.5523 
-                      19.3749 22 19.8227 22 20.3749C22 20.9272 21.5523 21.3749 21 21.3749H4C3.44772 21.3749 3 
-                      20.9272 3 20.3749V15.3749C3 15.1097 3.10536 14.8554 3.29289 14.6678L13.8787 4.08205ZM8.58579 
-                      19.3749L16.5858 11.3749L13 7.78916L5 15.7892V19.3749H8.58579ZM14.4142 6.37494L18 9.96073L18.8787
-                      9.08205C19.2692 8.69152 19.2692 8.05836 18.8787 7.66784L16.7071 5.49626C16.3166 5.10574 15.6834 
-                      5.10574 15.2929 5.49626L14.4142 6.37494Z"
-                      fill="black"
-                    />
-                  </svg>
+                    onClick={() => {
+                      setStatusTel(!statusTel);
+                    }}
+                  />
                 </div>
               </div>
             </div>
@@ -216,8 +192,7 @@ export default function Profile() {
               shadow-sm text-white text-bold right-0 bottom-0 mt-4"
                 onClick={() => {
                   if (id && session.data.user.token) {
-                    updateUser(id, name, email, session.data.user.token);
-                    alert("User information saved successfully!");
+                    updateUser(id, name, email, tel, session.data.user.token);
                   }
                 }}
               >
