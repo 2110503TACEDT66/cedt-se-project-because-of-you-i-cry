ad8a4d50b0aca5962cad2e77d05ecef07f16dc4e
TenJit <tenjitrattanakul@gmail.com>
2024-04-16 06:08:26 +0700

    new campground detail

--- Frontend/src/app/(campgroundinfo)/campground/[id]/page.tsx
+++ Frontend/src/app/(campgroundinfo)/campground/[id]/page.tsx
@@ -1,163 +1,347 @@
 "use client";
+
+import { useEffect, useState, useCallback } from "react";
 import Image from "next/image";
 import getCampground from "@/libs/getCampground";
 import DateReserve from "@/components/DateReserve";
 import Link from "next/link";
-import { Rating } from "@mui/material";
+import { FormControl, Rating } from "@mui/material";
 import Available from "@/components/Available";
-// import ReserveCampground from "@/components/ReserveCampground";
-// import { addBooking } from "@/redux/features/bookSlice";
+import styles from "./page.module.css";
+import LinkIcon from "@mui/icons-material/Link";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import SuccessModal from "@/components/SuccessModel";
+import { useSession } from "next-auth/react";
+import createBooking from "@/libs/createBooking";
+import { useRouter } from "next/navigation";
+import dayjs, { Dayjs } from "dayjs";
+import { useSearchParams } from "next/navigation";
+import {
+  CampgroundJson,
+  CampgroundItem,
+  CreateBookingItem,
+  UserJson,
+} from "../../../../../interface";
+import CommentCard from "@/components/CommentCard/CommentCard";
+import CommentPanel from "@/components/CommentPanel/CommentPanel";
 
-export default async function CampgroundDetailPage({
+export default function CampgroundDetailPage({
   params,
 }: {
   params: { id: string };
 }) {
-  const campgroundReady = await getCampground(params.id);
+  const [campgroundReady, setCampgroundReady] = useState<any>(null);
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const session = useSession();
+  const router = useRouter();
+  const urlParams = useSearchParams();
+  const id = urlParams.get(`id`);
+  const [checkin, setCheckin] = useState<Dayjs | null>(null);
+  const currentDate = dayjs();
+
+  const fetchCampground = useCallback(async () => {
+    const campgroundData = await getCampground(params.id);
+    setCampgroundReady(campgroundData);
+  }, [params.id]);
+
+  useEffect(() => {
+    fetchCampground();
+  }, [params.id, fetchCampground]);
+
+  const makeReservation = () => {
+    if (params.id && campgroundReady && session.data) {
+      try {
+        const item: CreateBookingItem = {
+          apptDate: dayjs(checkin).toISOString(),
+          user: session.data.user._id,
+          campground: params.id,
+        };
+        createBooking(item, session.data.user.token);
+      } catch (error) {
+        console.log(error);
+      }
+      // dispatch(addBooking(item));
+      // router.push("/success");
+    } else {
+      alert("Please Choose your campground to reserve");
+    }
+  };
 
   if (!campgroundReady) return <p>Campground Loading ...</p>;
 
-  let pic = 0;
-  if (campgroundReady.data && Array.isArray(campgroundReady.data.picture)) {
-    pic = campgroundReady.data.picture.length;
-  }
-  let i = 0;
+
+  const commentsData = [
+    {
+      name: "Name of comment's owner",
+      rating: 5,
+      comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+    },
+    {
+      name: "Bob sleepy af helpp meeeeeeeeeeeeeeeeeeeeeeeeeeee",
+      rating: 4,
+      comment: "Great product! Really enjoyed using it. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamLorem ipsum dolor sit amet, consectetur adipiscing  et dolore magna aliqua. Ut enim ad minim veniam",
+    },
+    {
+      name: "Charlie",
+      rating: 3,
+      comment: "It's good, but there is room for improvement.",
+    },
+    {
+      name: "Dana",
+      rating: 2,
+      comment: "Not very satisfied with the quality.",
+    },
+    {
+      name: "Eve",
+      rating: 4,
+      comment: "Nice experience overall, but a bit pricey.",
+    },
+    {
+      name: "Frank",
+      rating: 5,
+      comment: "An amazing product! I highly recommend it.",
+    },
+    {
+      name: "Grace",
+      rating: 3,
+      comment: "Decent quality, but not quite what I expected.",
+    },
+    {
+      name: "Hank",
+      rating: 4,
+      comment: "Good value for the price, will buy again.",
+    },
+    {
+      name: "Ivy",
+      rating: 2,
+      comment: "Disappointed with the performance.",
+    },
+    {
+      name: "Jack",
+      rating: 5,
+      comment: "Top-notch! Can't wait to order again.",
+    },
+    {
+      name: "Karen",
+      rating: 3,
+      comment: "Okay, but not great.",
+    },
+    {
+      name: "Leo",
+      rating: 4,
+      comment: "Really nice product, but could be better.",
+    },
+    {
+      name: "Mona",
+      rating: 5,
+      comment: "Simply outstanding!",
+    },
+    {
+      name: "Nina",
+      rating: 2,
+      comment: "Not impressed with the quality.",
+    },
+    {
+      name: "Oscar",
+      rating: 4,
+      comment: "Good, but there's some room for improvement.",
+    },
+    {
+      name: "Paula",
+      rating: 5,
+      comment: "Exceeded my expectations! Highly recommend.",
+    },
+    {
+      name: "Quinn",
+      rating: 3,
+      comment: "It's okay, but I've seen better.",
+    },
+    {
+      name: "Rachel",
+      rating: 4,
+      comment: "Good quality, satisfied with my purchase.",
+    },
+    {
+      name: "Sam",
+      rating: 5,
+      comment: "Fantastic! Loved every bit of it.",
+    },
+    {
+      name: "Tina",
+      rating: 2,
+      comment: "Poor performance, not worth the money.",
+    },
+    {
+      name: "Uma",
+      rating: 4,
+      comment: "Overall, a good experience.",
+    },
+    {
+      name: "Vince",
+      rating: 3,
+      comment: "Average product, nothing special.",
+    },
+    {
+      name: "Wendy",
+      rating: 5,
+      comment: "Exceptional quality! Will buy again.",
+    },
+    {
+      name: "Xavier",
+      rating: 4,
+      comment: "Nice product, but some improvements needed.",
+    },
+    {
+      name: "Yara",
+      rating: 3,
+      comment: "Decent, but not outstanding.",
+    },
+    {
+      name: "Zane",
+      rating: 5,
+      comment: "Excellent product! Highly recommend.",
+    },
+  ];
+
+  
 
   return (
-    <main className="block w-full h-auto flex flex-col justify-center space-y-16 my-10 items-center text-black">
-      <div className="w-[90%] h-[70vh] border-4 border-bg-black flex flex-col justify-center  space-y-5 top-1/2 rounded-2xl p-5 items-center">
-        <div className="w-[100%] h-[30%] flex flex-row h-auto justify-center items-center">
-          <div className="block items-left w-[45%] space-y-2">
-            <div className="text-4xl flex items-center">
-              <span>{campgroundReady.data.name}</span>
-              <span className="text-sky-500 text-sm flex flex-row ml-2">
-                <a href={campgroundReady.data.url}>Visit website</a>
-                <Image
-                  src={"/img/link.png"}
-                  alt="location"
-                  width={15}
-                  height={8}
-                  className="ml-2"
-                />
-              </span>
+    <div className={styles.page}>
+      <div className={styles.campgroundBlock}>
+        <div className={styles.rowBlock}>
+          <div className={styles.campgroundDetail}>
+            <div className={styles.rowBlock}>
+              <div className={styles.name}>{campgroundReady.data.name}</div>
+              <div className={styles.link}>
+                <a href={campgroundReady.data.url}>
+                  Visit website{" "}
+                  <LinkIcon style={{ transform: "rotate(135deg)" , fontSize: "1.1rem" }} />
+                </a>
+              </div>
             </div>
-
-            <div className="text-lg items-end content-end">
-              Rating:{" "}
+            <div className={styles.rating}>
               <Rating
                 name="camground rating"
                 defaultValue={campgroundReady.data.rating}
-                max={campgroundReady.data.rating}
+                max={5}
                 readOnly
               />
             </div>
-            <div className="">
-              <div className="flex items-center">
-                <img
-                  src="/img/location.png"
-                  alt="location"
-                  className="mr-2"
-                  style={{ width: "20px", height: "20px" }}
-                />
-                <span>
-                  Address: {campgroundReady.data.address}{" "}
-                  {campgroundReady.data.district}{" "}
-                  {campgroundReady.data.postalcode}
-                </span>
-              </div>
-              <div className="flex items-center">
-                <img
-                  src="/img/tel.png"
-                  alt="tel"
-                  className="mr-2"
-                  style={{ width: "15px", height: "15px" }}
-                />
-                <span>
-                  <div>Tel: {campgroundReady.data.tel}</div>
-                </span>
-              </div>
+            <div className={styles.address}>
+              <LocationOnIcon style={{ color: "#222222", opacity: 1 }} />
+              {campgroundReady.data.address}
+              {", "} {campgroundReady.data.district}
+              {", "}
+              {campgroundReady.data.province} {campgroundReady.data.postalcode}
+              {", "}
+              {campgroundReady.data.region}
             </div>
           </div>
-          <div className="top-0 right-0 h-[100%] w-[45%] text-2xl items-end text-right text-end">
-            <h1 className="h-full"> {campgroundReady.data.price} Baht</h1>
+          <div className={styles.priceBlock}>
+            <div className={styles.priceHead}>Avg per night</div>
+            <div className={styles.price}>THB {campgroundReady.data.price}</div>
           </div>
         </div>
-        <div className="w-[100%] h-[70%] flex flex-row h-auto justify-center items-center">
-          <div className="w-[45%] h-[40vh] rounded-lg p-3 mx-3 my-auto flex items-center justify-center relative">
-            <Image
-              src={campgroundReady.data.coverpicture}
-              alt="campground picture0"
-              layout="fill"
-              objectFit="fill"
-              className="rounded-lg relative"
-            />
+        <div className={styles.rowBlock}>
+          <div className={styles.pictureBlock}>
+            <Swiper
+              modules={[Navigation, Pagination, Scrollbar, A11y]}
+              spaceBetween={50}
+              slidesPerView={1}
+              navigation
+              pagination={{
+                dynamicBullets: true,
+                clickable: true,
+                //currentClass: 'pagination',
+                //el: '.swiper-pagination-custom'
+              }}
+            >
+              <div>
+                <SwiperSlide className={styles.slideBlock}>
+                  <Image
+                    src={campgroundReady.data.coverpicture}
+                    alt={"Campground Image 0"}
+                    width={10000}
+                    height={0}
+                    className={styles.sliderImage}
+                  />
+                </SwiperSlide>
+                {campgroundReady.data.picture.map(
+                  (imgSrc: string, index: number) => (
+                    <SwiperSlide className={styles.slideBlock}>
+                      <Image
+                        src={imgSrc}
+                        alt={`Campground Image ${index + 1}`}
+                        width={10000}
+                        height={0}
+                        className={styles.sliderImage}
+                      />
+                    </SwiperSlide>
+                  )
+                )}
+              </div>
+            </Swiper>
           </div>
-          <div className="w-[45%] h-[40vh] rounded-lg p-3 mx-3 my-auto flex flex-col items-center justify-center relative">
-            <div className="w-[100%] h-[20vh] rounded-lg mx-3 my-2 flex flex-row items-center justify-center relative">
-              <div className=" w-[50%] h-full rounded-lg relative m-2">
-                <Image
-                  src={
-                    i < pic
-                      ? campgroundReady.data.picture[i++]
-                      : "/img/card2.jpg"
-                  }
-                  alt="campground picture1"
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-lg relative"
-                />
-              </div>
-              <div className="w-[50%] h-full rounded-lg relative">
-                <Image
-                  src={
-                    i < pic
-                      ? campgroundReady.data.picture[i++]
-                      : "/img/card2.jpg"
-                  }
-                  alt="campground picture2"
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-lg relative"
-                />
-              </div>
+          <div className={styles.descriptionBlock}>
+            <div className={styles.description}>
+              {campgroundReady.data.description}
             </div>
-            <div className="w-[100%] h-[20vh] rounded-lg mx-3 my-auto flex flex-row items-center justify-center relative">
-              <div className=" w-[50%] h-full rounded-lg relative m-2">
-                <Image
-                  src={
-                    i < pic
-                      ? campgroundReady.data.picture[i++]
-                      : "/img/card2.jpg"
-                  }
-                  alt="campground picture3"
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-lg relative"
-                />
+            <FormControl className={styles.rowBlock2}>
+              <div className={styles.rowBlock}>
+                <div className={styles.checkInBlock}>
+                  Check In
+                  <div className={styles.dateReserve}>
+                    <DateReserve
+                      onDateChange={(value: Dayjs) => {
+                        if (value.isAfter(currentDate)) {
+                          //alert("set Check IN")
+                          setCheckin(value);
+                        } else {
+                          alert(
+                            "Please select a date other than today and before today."
+                          );
+                        }
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className={styles.buttonBlock}>
+                  <button
+                    type="submit"
+                    name="Book Campground"
+                    className={styles.reserveButton}
+                    onClick={() => {
+                      if (checkin) {
+                        setShowSuccessModal(true);
+                        makeReservation();
+                      } else {
+                        alert("Please pick a date to reserve. ");
+                      }
+                    }}
+                  >
+                    Reserve
+                  </button>
+                </div>
               </div>
-              <div className="w-[50%] h-full rounded-lg relative">
-                <Image
-                  src={
-                    i < pic
-                      ? campgroundReady.data.picture[i++]
-                      : "/img/card2.jpg"
-                  }
-                  alt="campground picture4"
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-lg relative"
-                />
-              </div>
-            </div>
+              <SuccessModal
+                isOpen={showSuccessModal}
+                onClose={() => setShowSuccessModal(false)}
+              />
+            </FormControl>
           </div>
         </div>
+      </div> 
+      <div className={styles.commentText}>
+        Comment
       </div>
-      <div className="w-[90%] h-[30vh] flex flex-row justify-center space-x-5 rounded-2xl p-5 items-center">
-        <div className="w-[100%] h-[35vh] border-4 border-bg-black flex flex-col justify-center rounded-2xl m-5 items-center text-left text-lg text-black p-10">
-          {campgroundReady.data.description}
-        </div>
-        <Available id={params.id} />
-      </div>
-    </main>
+      <CommentPanel CommentArray={commentsData}/>
+
+    </div>
   );
 }
---
+++ Frontend/src/components/CommentPanel/CommentPanel.tsx
@@ -0,0 +1,58 @@
+import styles from "./commentpanel.module.css";
+import CommentCard from "../CommentCard/CommentCard";
+import { useState } from "react";
+import AddCommentCard from "../AddCommentCard/AddCommentCard";
+
+export default function CommentPanel({ CommentArray }: { CommentArray: any }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const commentsPerPage = 5;
+
+  // Calculate the index of the first and last comment to display
+  const indexOfLastComment = currentPage * commentsPerPage;
+  const indexOfFirstComment = indexOfLastComment - commentsPerPage;
+
+  // Get the comments to display on the current page
+  const currentComments = CommentArray.slice(
+    indexOfFirstComment,
+    indexOfLastComment
+  );
+
+  // Function to handle page change
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(CommentArray.length / commentsPerPage);
+
+  return (
+    <div className={styles.fullBlock}>
+      <div className={styles.addCommentBlock}>
+        <AddCommentCard/>
+      </div>
+      <div className={styles.commentBlock}>
+        {currentComments.map((comment: any, index: number) => (
+          <CommentCard
+            key={index}
+            name={comment.name}
+            rating={comment.rating}
+            comment={comment.comment}
+          />
+        ))}
+      </div>
+      <div className={styles.pagination}>
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+          <div
+            key={page}
+            className={`${styles.pageBullet} ${
+              currentPage === page ? styles.activeBullet : ""
+            }`}
+            onClick={() => handlePageChange(page)}
+          >
+            •
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
