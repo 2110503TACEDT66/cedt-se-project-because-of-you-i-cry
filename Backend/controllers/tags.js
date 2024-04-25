1f73f99687b69f844fc93ab6986fe4279826d9c4
Inthanon Ratnavardhana <inthanonratna@gmail.com>
2024-04-25 19:08:44 +0700

    add&remove TagToCampground

--- Backend/controllers/tags.js
+++ Backend/controllers/tags.js
@@ -1,3 +1,54 @@
 const Tag = require('../models/Tag');
 const Campground = require('../models/Campground');
 
+exports.addTagToCampground = async (req, res, next) => {
+
+    try {
+        
+        const campground = await Campground.findById(req.params.campgroundId);
+        const tag = await Tag.findById(req.params.tagId);
+
+        campground.tags.push(tag);
+        await campground.save();
+
+        res.status(200).json({success : true});
+
+    } catch (error) {
+
+        res.status(400).json({
+            success : false, 
+            message : error
+        });
+
+    }
+
+}
+
+exports.removeTagToCampground = async (req, res, next) => {
+
+    try {
+
+        const campground = await Campground.findById(req.params.campgroundId);
+        const tag = await campground.tags.findById(req.params.tagId);
+
+        if ( !tag ) {
+            return res.status(404).json({
+                success: false,
+                message: `Cannot find a tag with id ${req.params.tagId}`,
+              });
+        }
+
+        await tag.deleteOne();
+        res.status(200).json({ success: true });
+
+    } catch (error) {
+        
+        res.status(400).json({
+            success : false, 
+            message : error
+        });
+
+
+    }
+
+}
