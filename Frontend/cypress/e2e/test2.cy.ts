35fc5770aa58ee336869c99f97d8f46073e4b0ad
NATCH <nat.siriruangbun@gmail.com>
2024-05-01 06:43:58 +0700

    Finish US 101

--- Frontend/cypress/e2e/test2.cy.ts
+++ Frontend/cypress/e2e/test2.cy.ts
@@ -20,4 +20,41 @@ describe('User Story 1-2', () => {
         cy.wait(3000)
         cy.get('[data-test="TrendingNow"]').find('[data-test="SliderCard"]').should('have.length' , 3)
         
-    })})
+    })
+
+    it('US1-2-T2' , () => {
+
+        cy.intercept('GET','http://localhost:1234/api-informations/campgrounds?sort=-rating',
+            {
+                statusCode: 200,
+                fixture: "exact3Campground"
+            }
+        ).as('MockOneCampgroundWithRating')
+
+        cy.visit('http://localhost:3000/')
+
+        cy.wait(3000)
+        cy.get('[data-test="TrendingNow"]').find('[data-test="SliderCard"]').should('have.length' , 3)
+        
+    })
+
+    it('US1-2-T3' , () => {
+
+        cy.intercept('GET','http://localhost:1234/api-informations/campgrounds?sort=-rating',
+            {
+                statusCode: 200,
+                fixture: "lessThan3Campground"
+            }
+        ).as('MockOneCampgroundWithRating')
+
+        cy.visit('http://localhost:3000/')
+
+        cy.wait(3000)
+        cy.get('[data-test="TrendingNow"]').find('[data-test="SliderCard"]').should('have.length' , 2)
+        
+    })
+
+
+
+
+})
