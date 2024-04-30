383e3c10f90d0d9a833fd9763f472a3e460a1ed0
NATCH <nat.siriruangbun@gmail.com>
2024-04-30 13:22:10 +0700

    Fix Trending now

--- Frontend/src/components/ImageSlider.tsx
+++ Frontend/src/components/ImageSlider.tsx
@@ -93,42 +93,52 @@ export default function ImageSlider({
   }
 
   // Fill campgroundArray with mocknone until it reaches a length of 3
-  while (campgroundArray.length < 3) {
-    campgroundArray.push(mocknone);
-  }
+  // while (campgroundArray.length < 3) {
+  //   campgroundArray.push(mocknone);
+  // }
+
+  const campgroundArraySlice = campgroundArray.slice(0, 3);
+
+
 
   return (
-    <div className={styles.SliderWrapper}>
-      <Swiper
-        style={style}
-        slidesPerView={3}
-        spaceBetween={0}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        loop={true}
-        className="mySwiper"
-      >
-        {campgroundArray.map((campground: any, index: number) => (
-          <SwiperSlide key={index}>
-            {campground.name === "none" ? (
-              <div className="flex items-center justify-center">
-                <div
-                  className="h-[296px] w-[342px] overflow-hidden rounded-[15px] relative cursor-pointer bg-[#D9D9D9]
-                                flex flex-row justify-center items-center wrap"
-                >
-                  <div className="w-[80%] wrap text-center">
-                    "There are no more campgrounds related to this filter"
-                  </div>
-                </div>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center">
-                <SliderCard data={campground} />
-              </div>
-            )}
-          </SwiperSlide>
+    // <div className={styles.SliderWrapper}>
+    //   <Swiper
+    //     style={style}
+    //     slidesPerView={3}
+    //     spaceBetween={0}
+    //     navigation={true}
+    //     modules={[Pagination, Navigation]}
+    //     loop={true}
+    //     className="mySwiper"
+    //   >
+    //     {campgroundArray.map((campground: any, index: number) => (
+    //       <SwiperSlide key={index}>
+    //         {campground.name === "none" ? (
+    //           <div className="flex items-center justify-center">
+    //             <div
+    //               className="h-[296px] w-[342px] overflow-hidden rounded-[15px] relative cursor-pointer bg-[#D9D9D9]
+    //                             flex flex-row justify-center items-center wrap"
+    //             >
+    //               <div className="w-[80%] wrap text-center">
+    //                 "There are no more campgrounds related to this filter"
+    //               </div>
+    //             </div>
+    //           </div>
+    //         ) : (
+    //           <div className="flex items-center justify-center">
+    //             <SliderCard data={campground} />
+    //           </div>
+    //         )}
+    //       </SwiperSlide>
+    //     ))}
+    //   </Swiper>
+    // </div>
+
+    <div className="mt-[5%] px-[7%] w-[100%] flex flex-row items-center justify-between">
+        {campgroundArraySlice.map((campground : any) => (
+          <SliderCard data={campground}/>
         ))}
-      </Swiper>
     </div>
   );
 }
