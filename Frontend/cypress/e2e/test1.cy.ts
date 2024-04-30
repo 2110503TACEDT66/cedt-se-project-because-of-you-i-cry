0ce3e09750e09012bba179f7ef3fff1ede0f9495
KpSuphakorn <suphakorn04413@gmail.com>
2024-05-01 06:31:57 +0700

    finish 1-3

--- Frontend/cypress/e2e/test1.cy.ts
+++ Frontend/cypress/e2e/test1.cy.ts
@@ -10,40 +10,131 @@ describe('User Story 1-3', () => {
     cy.viewport(1440, 900);
   });
 
-  it('US1-3-T4' , () => {
+//   it('US1-3-T1' , () => {
+  
+//     //HomePage
+//     cy.visit('http://localhost:3000')
 
-    //HomePage
-    cy.visit('http://localhost:3000')
+//     cy.get('a[href="/campground"]').first().click()
+//     cy.wait(5000)
 
-    cy.get('a[href="/api/auth/login"]').first().click()
-    cy.wait(5000)
+//     //AllCampground Page
+//     cy.get('div').contains('Thammachard').click()
+//     cy.wait(5000)
 
-    //Register Page
-    cy.get('input').eq(0).type("ten@gmail.com")
-    cy.get('input').eq(1).type("123456")
-    cy.get('button').contains('Sign In').click()
-    cy.wait(5000)
+//     //Phu Tub Berk Page
+//     cy.get('input[value=4]').click({ force: true })
+//     cy.get('[data-test="addCommentBlock"]').type('This is a test From US1-3-T1') // Type the comment text
+//       cy.get('button').contains('Add').click() // Click the "Add" button
+//       cy.wait(5000)
 
-    cy.get('a[href="/campground"]').first().click()
-    cy.wait(5000)
+//     cy.on('window:alert',(alert)=>{
+//       //Assertion
+//       expect(alert).to.contains('Error creating comment')
+//       })
+//       cy.wait(5000)
+//     })
+
+//     it("US1-3-T2", () => {
+//         //HomePage
+//         cy.visit("http://localhost:3000");
+    
+//         cy.get('a[href="/api/auth/login"]').first().click();
+//         cy.wait(5000);
+    
+//         //Register Page
+//         cy.get("input").eq(0).type("kkk@gmail.com");
+//         cy.get("input").eq(1).type("123456");
+//         cy.get("button").contains("Sign In").click();
+//         cy.wait(5000);
+    
+//         cy.get('a[href="/campground"]').first().click();
+//         cy.wait(5000);
+    
+//         //AllCampground Page
+//         cy.get("div").contains("Phu Tub Berk").click();
+//         cy.wait(5000);
+    
+//         //Phu Tub Berk Page
+//         cy.get("input[value=4]").click({ force: true });
+//         cy.get('[data-test="addCommentBlock"]').type("This is a test From US1-3-T2"); // Type the comment text
+//         cy.get("button").contains("Add").click(); // Click the "Add" button
+//         cy.wait(5000);
+    
+//         cy.on("window:alert", (alert) => {
+//           //Assertion
+//           expect(alert).to.contains("Error creating comment");
+//         });
+//         cy.wait(5000);
+//     });
+
+//     it("US1-3-T3", () => {
+//         //HomePage
+//         cy.visit("http://localhost:3000");
+    
+//         cy.get('a[href="/api/auth/login"]').first().click();
+//         cy.wait(5000);
+    
+//         //Register Page
+//         cy.get("input").eq(0).type("kkk@gmail.com");
+//         cy.get("input").eq(1).type("123456");
+//         cy.get("button").contains("Sign In").click();
+//         cy.wait(5000);
+    
+//         cy.get('a[href="/campground"]').first().click();
+//         cy.wait(5000);
+    
+//         //AllCampground Page
+//         cy.get("div").contains("Thammachard").click();
+//         cy.wait(5000);
+    
+//         //Phu Tub Berk Page
+//         cy.get("input[value=4]").click({ force: true });
+//         cy.get('[data-test="addCommentBlock"]').type("This is a test From US1-3-T3"); // Type the comment text
+//         cy.get("button").contains("Add").click(); // Click the "Add" button
+//         cy.wait(5000);
+    
+//         cy.on("window:alert", (alert) => {
+//           //Assertion
+//           expect(alert).to.contains("Error creating comment");
+//         });
+//         cy.wait(5000);
+//     });
+
+//   it('US1-3-T4' , () => {
+
+//     //HomePage
+//     cy.visit('http://localhost:3000')
+
+//     cy.get('a[href="/api/auth/login"]').first().click()
+//     cy.wait(5000)
+
+//     //Register Page
+//     cy.get('input').eq(0).type("ten@gmail.com")
+//     cy.get('input').eq(1).type("123456")
+//     cy.get('button').contains('Sign In').click()
+//     cy.wait(5000)
+
+//     cy.get('a[href="/campground"]').first().click()
+//     cy.wait(5000)
 
 
 
-    //AllCampground Page
-    cy.get('div').contains('Thammachard').click()
-    cy.wait(5000)
+//     //AllCampground Page
+//     cy.get('div').contains('Thammachard').click()
+//     cy.wait(5000)
 
-    //Phu Tub Berk Page
-    cy.get('[data-test="addCommentBlock"]').type('This is a test commenttt') // Type the comment text
-      cy.get('button').contains('Add').click() // Click the "Add" button
-      cy.wait(5000)
+//     //Phu Tub Berk Page
+//     cy.get('[data-test="addCommentBlock"]').type('This is a test From US1-3-T4') // Type the comment text
+//       cy.get('button').contains('Add').click() // Click the "Add" button
+//       cy.wait(5000)
 
-    cy.on('window:alert',(alert)=>{
-      //Assertion
-      expect(alert).to.contains('Error creating comment')
-      })
-      cy.wait(5000)
-    })
+//     cy.on('window:alert',(alert)=>{
+//       //Assertion
+//       expect(alert).to.contains('Error creating comment')
+//       })
+//       cy.wait(5000)
+//     })
 
     it('US1-3-T5', () => {
         // HomePage
@@ -64,15 +155,21 @@ describe('User Story 1-3', () => {
         cy.get('div').contains('Thammachard').click();
         cy.wait(5000);
     
-        // Phu Tub Berk Page - First Round
+        // Phu Tub Berk Page
         cy.get('input[value=4]').click({ force: true }); // Click the 4-star rating
-        cy.get('[data-test="addCommentBlock"]').type('This is first test comment'); // Type the comment text
+        cy.get('[data-test="addCommentBlock"]').type('This is Doraemon test comment555'); // Type the comment text
         cy.get('button').contains('Add').click(); // Click the "Add" button
         cy.wait(5000);
     
-        cy.on('window:alert', (alert) => {
-          // Assertion
-          expect(alert).to.contains('Error creating comment');
+        cy.get('a[href="/campground"]').first().click();
+        cy.wait(5000);
+    
+        // AllCampground Page
+        cy.get('div').contains('Thammachard').click();
+        cy.wait(5000);
+
+        cy.get("div").contains("This is Doraemon test comment555").should(($div) => {
+            expect($div).to.exist;
         });
       });
 
@@ -97,7 +194,7 @@ describe('User Story 1-3', () => {
 
     // Phu Tub Berk Page - First Round
     cy.get('input[value=4]').click({ force: true }); // Click the 4-star rating
-    cy.get('[data-test="addCommentBlock"]').type('This is first test comment'); // Type the comment text
+    cy.get('[data-test="addCommentBlock"]').type('This is first test comment666'); // Type the comment text
     cy.get('button').contains('Add').click(); // Click the "Add" button
     cy.wait(5000);
 
@@ -107,14 +204,23 @@ describe('User Story 1-3', () => {
     });
 
     // Phu Tub Berk Page - Second Round
-    cy.get('input[value=3]').click({ force: true }); // Click the 3-star rating
-    cy.get('[data-test="addCommentBlock"]').clear().type('This is another test comment'); // Change the comment text
+    cy.get('input[value=5]').click({ force: true }); // Click the 3-star rating
+    cy.get('[data-test="addCommentBlock"]').clear().type('This is another test comment777'); // Change the comment text
     cy.get('button').contains('Add').click(); // Click the "Add" button
     cy.wait(5000);
 
-    cy.on('window:alert', (alert) => {
-      // Assertion
-      expect(alert).to.contains('Error creating comment');
+    cy.get('a[href="/campground"]').first().click();
+    cy.wait(5000);
+
+    // AllCampground Page
+    cy.get('div').contains('Thammachard').click();
+    cy.wait(5000);
+
+    cy.get("div").contains("This is first test comment666").should(($div) => {
+        expect($div).to.exist;
+    });
+    cy.get("div").contains("This is another test comment777").should(($div) => {
+        expect($div).to.exist;
     });
   });
 });
